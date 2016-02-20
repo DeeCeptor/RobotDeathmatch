4d6947be8fe01742aa686e2af2ad6fd094b166d9
jon <jov298@mail.usask.ca>
2016-02-20 16:27:28 -0600

    no message

---
+++ RobotDeathmatch/Assets/Scripts/Pause.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	Font pausemenufont;
+	bool ispaused = false;
+	public Canvas pausedscreen; 
+
+	// Use this for initialization
+	void Start () {
+		GUI.skin.box.font = pausemenufont;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Pause") && ispaused == false) {
+			Time.timeScale = 0f;
+			ispaused = true;
+			pausedscreen.enabled = true;
+		} else if (Input.GetButtonDown ("Pause") && ispaused == true) {
+			Time.timeScale = 1f;
+			ispaused = false;
+			pausedscreen.enabled=false;
+		}
+	}
+
+	}
+
