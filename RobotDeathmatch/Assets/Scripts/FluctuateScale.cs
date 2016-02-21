9c16a4033fb10a31909c40c29114638328a5b1fc
Michael Long <sirmichaellong@gmail.com>
2016-02-20 21:35:55 -0600

    no message

---
+++ RobotDeathmatch/Assets/Scripts/FluctuateScale.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluctuateScale : MonoBehaviour
+{
+    private bool shrinking = true;
+    float lower_range = 0.7f;
+    float scale_rate = 0.5f;
+
+	void Start () {
+	
+	}
+
+    void OnEnable()
+    {
+        this.transform.localScale = Vector3.one;
+    }
+
+    void Update ()
+    {
+        Vector2 cur_scale = this.transform.localScale;
+	    if (shrinking)
+        {
+            this.transform.localScale = new Vector3(cur_scale.x - Time.deltaTime * scale_rate,
+                cur_scale.x - Time.deltaTime * scale_rate, 1);
+
+            if (this.transform.localScale.x < lower_range)
+                shrinking = false;
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(cur_scale.x + Time.deltaTime * scale_rate,
+                cur_scale.x + Time.deltaTime * scale_rate, 1);
+
+            if (this.transform.localScale.x >= 1)
+                shrinking = true;
+        }
+  	}
+}
---
+++ RobotDeathmatch/Assets/Scripts/ScaleUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleUp : MonoBehaviour
+{
+    public float rate = 1;
+
+
+    void Update ()
+    {
+        Vector2 local_scale = this.transform.localScale;
+        this.transform.localScale = new Vector3(local_scale.x + Time.deltaTime * rate,
+            local_scale.x + Time.deltaTime * rate, 1);
+	}
+}
