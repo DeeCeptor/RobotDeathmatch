661343e5063fb88c063741d161f345b26f47a944
Mike Bzowy <mikebzowy@gmail.com>
2016-02-20 18:43:52 -0600

    ui stuff

---
+++ RobotDeathmatch/Assets/Scripts/Interface/LevelSelectScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectScreen : MonoBehaviour {
+	public GameObject slot0;
+	public GameObject slot1;
+	public GameObject slot2;
+	public GameObject slot3;
+	public GameObject slot4;
+	public GameObject slot5;
+
+	int current = 0;
+	public float delay;
+	float moveTime;
+	// Use this for initialization
+	void Start () {
+		moveTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > moveTime + delay) {
+			float vertical_movement = Input.GetAxis ("P1 LeftStick Y");
+		}
+	}
+}
---
+++ RobotDeathmatch/Assets/Scripts/Interface/levelItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelItem : MonoBehaviour {
+
+	public Sprite deselected;
+	public Sprite selected;
+
+	SpriteRenderer render;
+	// Use this for initialization
+	void Start () {
+		render = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Select(){
+		render.sprite = selected;
+	}
+
+	public void Deselect(){
+		render.sprite = deselected;
+	}
+}
