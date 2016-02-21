5f6678e9c70251c473ae1402b5e8707d6e470f2b
Mike Bzowy <mikebzowy@gmail.com>
2016-02-21 09:19:47 -0600

    h

---
+++ RobotDeathmatch/Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusic : MonoBehaviour {
+	private static MenuMusic instance = null;
+	public static MenuMusic Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public static void stopMusic() {
+		Destroy (instance.gameObject);
+		instance = null;
+	}
+
+}
